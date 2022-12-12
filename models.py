3e5bd3eaf0cd397553caa7bf9f10b034433b1e38
unknown <leonfortgens@hotmail.se>
2022-12-12 01:09:40 +0100

    Added a cascade and played around with it

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine("sqlite///zoo.db", echo=False)
+engine = create_engine("sqlite:///zoo.db", echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
@@ -20,7 +20,8 @@ class Animal(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     habitat = Column(String)
-    logs = relationship("Logbook", back_populate="animal")
+    logs = relationship("Logbook", back_populates="animal",
+                        cascade="all,delete, delete-orphan")
 
 
     def __repr__(self):
@@ -29,7 +30,6 @@ class Animal(Base):
             Name = {self.name}\r
             Habitat = {self.habitat}
             '''
-
 
 
 
@@ -47,3 +47,8 @@ class Logbook(Base):
                 Animal ID = {self.animal_id}\r
                 Notes = {self.notes}
             '''
+
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
